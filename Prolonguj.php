f9018e41482ec838ef1c4d64503266c4fa4e0a14
Lemonhorse144 <fanofsubnautica@gmail.com>
2024-03-12 23:24:28 +0100

    prolong zwrot

---
+++ Prolonguj.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8" />
+    <title>Prolonguj wypożyczenie</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+
+<body>
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $num = $_POST["num"];
+
+        $polaczenie = new mysqli("localhost", "admin", "admin", "biblioteka");
+
+        if ($polaczenie->connect_error) {
+            die("Błąd połączenia z bazą danych: " . $polaczenie->connect_error);
+        }
+
+        // Pobierz obecną datę
+        $termin = date("Y-m-d", strtotime("+1 month"));
+
+        // Wykonaj kwerendę modyfikującą rekord w tabeli wypozyczenia
+        $sql_update = "UPDATE wypozyczenia SET zwrot = '$termin' WHERE num = '$num'";
+
+        if ($polaczenie->query($sql_update) === TRUE) {
+            echo "<h1>Prolongowano wypożyczenie książki do: $termin</h1>";
+        } else {
+            echo "<h1>Błąd podczas prolongowania wypożyczenia: " . $polaczenie->error . "</h1>";
+        }
+
+        $polaczenie->close();
+    }
+    ?>
+    <br>
+    <a href='javascript:history.back()'>Wróć</a>
+</body>
+
+</html>
---
+++ Zwroc.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8" />
+    <title>Zwrot książki</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+
+<body>
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $num = $_POST["num"];
+
+        $polaczenie = new mysqli("localhost", "admin", "admin", "biblioteka");
+
+        if ($polaczenie->connect_error) {
+            die("Błąd połączenia z bazą danych: " . $polaczenie->connect_error);
+        }
+
+        // Pobierz obecną datę
+        $data_oddania = date("Y-m-d");
+
+        // Wykonaj kwerendę modyfikującą rekord w tabeli wypozyczenia
+        $sql_update = "UPDATE wypozyczenia SET oddano = '$data_oddania' WHERE num = '$num'";
+
+        if ($polaczenie->query($sql_update) === TRUE) {
+            echo "<h1>Książka została zwrócona pomyślnie.</h1>";
+        } else {
+            echo "<h1>Błąd podczas zwracania książki: " . $polaczenie->error . "</h1>";
+        }
+
+        $polaczenie->close();
+    }
+    ?>
+    <br>
+    <a href='javascript:history.back()'>Wróć</a>
+</body>
+
+</html>
