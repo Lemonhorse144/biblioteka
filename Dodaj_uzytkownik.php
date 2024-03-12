fbc9ca258c91a10e6419c786698257d49d577681
Lemonhorse144 <fanofsubnautica@gmail.com>
2024-03-12 21:43:56 +0100

    style

--- Dodaj_uzytkownik.php
+++ Dodaj_uzytkownik.php
@@ -10,8 +10,8 @@
         <div>
             <button onclick="location.href='Dodaj_wypozyczenie.html'">Wypożycz</button>
             <button onclick="location.href='Wyswietl_wypozyczenia.html'">Zarządzaj</button>
-            <button onclick="location.href='Dodaj_uzytkownik.html'">Dodaj czytelnika</button>
             <button onclick="location.href='Dodaj_ksiazka.html'">Dodaj książkę</button>
+            <button onclick="location.href='Dodaj_uzytkownik.html'">Dodaj czytelnika</button>
         </div>
     </nav>
     <h1>Potwierdzenie dodania użytkownika</h1>
--- Dodaj_wypozyczenie.php
+++ Dodaj_wypozyczenie.php
@@ -8,10 +8,10 @@
 <body>
     <nav>
         <div>
-            <button onclick="location.href='Dodaj_wypozyczenie.html'">Wypożycz</button>
             <button onclick="location.href='Wyswietl_wypozyczenia.html'">Zarządzaj</button>
             <button onclick="location.href='Dodaj_uzytkownik.html'">Dodaj czytelnika</button>
             <button onclick="location.href='Dodaj_ksiazka.html'">Dodaj książkę</button>
+            <button onclick="location.href='Dodaj_wypozyczenie.html'">Wypożycz</button>
         </div>
     </nav>
     <h1>Potwierdzenie wypożyczenia</h1>
